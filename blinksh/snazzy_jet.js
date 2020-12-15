a2cff51dbcbe1b120276f371ba5dab3835d62803
jeongjaepark <jeongjae0815@gmail.com>
2020-12-15 23:47:14 +0900

    First commit

---
+++ blinksh/snazzy_jet.js
@@ -0,0 +1,26 @@
+black       = '#000000';
+red         = '#fc4346'; // red
+green       = '#50fb7c'; // green
+yellow      = '#f0fb8c'; // yellow
+blue        = '#49baff'; // blue
+magenta     = '#fc4cb4'; // pink
+cyan        = '#8be9fe'; // cyan
+white       = '#ededec'; // light gray
+lightBlack  = '#555555'; // medium gray
+lightRed    = '#fc4346'; // red
+lightGreen  = '#50fb7c'; // green
+lightYellow = '#f0fb8c'; // yellow
+lightBlue   = '#49baff'; // blue
+lightMagenta= '#fc4cb4'; // pink
+lightCyan   = '#8be9fe'; // cyan
+lightWhite  = '#ededec'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('foreground-color', "#adadad");
+t.prefs_.set('background-color', "#202020");
+t.prefs_.set('cursor-color', 'rgba(187,187,187,0.5)');
